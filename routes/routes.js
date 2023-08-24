0f7c546f9b35dd2c577540c504cb5bd917ba60e3
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2023-08-24 17:11:32 +0700

    feat: implement REST API get, post, patch, delete

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/model');
 
-router.post('/post', (req, res) => {
-  res.send('Post API');
+router.post('/post', async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
-router.get('/get', (req, res) => {
-  res.send('Get all API');
+
+router.get('/get', async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-router.get('/get/:id', (req, res) => {
-  res.send(`Get by ID API. ID=${req.params.id}`);
+
+router.get('/get/:id', async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-router.patch('/update/:id', (req, res) => {
-  res.send('Update by ID API');
+
+router.patch('/update/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true};
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
-router.patch('/delete/:id', (req, res) => {
-  res.send(`Delete by ID API. ID=${req.params.id}`);
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.status(200).json({
+      message: `Document with ${data.name} has been deleted`
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
